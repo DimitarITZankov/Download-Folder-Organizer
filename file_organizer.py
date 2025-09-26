de0edb32ea76b4083e4d0ade4ac0044371671d30
dimitar <dimitarzankovit@gmail.com>
2025-09-26 12:26:12 +0300

    Preparing the categories

--- file_organizer.py
+++ file_organizer.py
@@ -3,3 +3,12 @@ import shutil
 import glob
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+categories = {
+    "Images" : ["jpg","png","gif"],
+    "Music" :  ["mp3","wav"],
+    "Videos" : ["mp4","mkv"],
+    "Documents" : ["doc","pdf","xlsx","txt"],
+    "Archives" : ["zip","rar","tar","gz"],
+    "Programs" : ["exe","msi","deb"]
+}
